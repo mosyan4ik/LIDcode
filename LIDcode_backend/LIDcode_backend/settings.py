e94af04005ae8941c2bfb9092e6f154d75b4dd48
Daniil <55555709+Mosyan4ick@users.noreply.github.com>
2023-03-23 00:01:37 +0300

    Update settings.py

--- LIDcode_backend/LIDcode_backend/settings.py
+++ LIDcode_backend/LIDcode_backend/settings.py
@@ -20,14 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-tgt2_@3_=v0tix$=#(!-+blf%b1%o(z_*(dxs&r$lh$25n(@l)'
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-# ALLOWED_HOSTS = []
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 # Application definition
